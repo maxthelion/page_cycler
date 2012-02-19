db6de703888fafeb519b730611fb84e3335d79cd
Max Williams <max@new-bamboo.co.uk>
2012-02-19 13:00:07 +0000

    Introduction of localStorage for settings

---
+++ app.js
@@ -0,0 +1,89 @@
+var timeOut = null;
+
+function fetchSettings(){
+  if (localStorage['URLData'])  
+    return JSON.parse( localStorage['URLData'] )
+}
+
+function drawMain(data){
+  $('#main').show();
+  $('#config').hide();
+  $('#current').attr('src', data[0].url)
+  $('#waiting').attr('src', data[0].url)
+  switchView(data)
+}
+
+function switchView(data){
+  var first = data.shift()
+  data.push(first)
+  var second = data[0]
+  if (second){
+    $('#heading').text(first.heading || '')
+    $('#waiting').css('height', '100%')
+    $('#current').css('height', '1px')
+    $('#waiting').attr('id', 'foo')
+    $('#current').attr('id', 'waiting').attr('src', second.url)
+    $('#foo').attr('id', 'current')
+    timeOut = setTimeout(function(){
+      switchView(data)
+    }, first.duration || 10000)
+  }
+}
+
+function saveData(){
+  var data = [];
+  $(this).find('.urlListItem').each(function(){
+    var dataPoint = {}
+    dataPoint.url = $(this).find('.urlField').val()
+    dataPoint.heading = $(this).find('.headingField').val()
+    dataPoint.duration = ($(this).find('.durationField').val() == "") ? 10000 : $(this).find('.durationField').val()
+    if (dataPoint.url != ""){
+      data.push(dataPoint)
+    }
+  })
+  localStorage['URLData'] = JSON.stringify(data)
+  drawMain(data)
+  return false
+}
+
+function showConfig(){
+  clearTimeout(timeOut)
+  $('#main').hide();
+  $('#config').show();
+  $('#urlList').empty();
+  var data = fetchSettings() || [];
+  for (var i=0; i < data.length; i++) {
+    addListItem( data[i] )
+  };
+  addListItem()
+  $('#config').submit(saveData)
+}
+
+function addListItem(item){
+  if (item)
+    $('#urlList').append('<div class="urlListItem"><input type="url" name="url" placeholder="URL" class="urlField" value="'+item.url+'"/>'+
+                          '<input name="heading" class="headingField" placeholder="LABEL" value="'+item.heading+'"/>'+
+                          '<input type="number" name="duration" class="durationField" placeholder="DURATION" value="'+(item.duration || 0)+'"/>'+
+                          '</div>')
+  else
+    $('#urlList').append('<div class="urlListItem"><input type="url" name="url" placeholder="URL" class="urlField" />'+
+                          '<input name="heading" class="headingField" placeholder="LABEL"/>'+
+                          '<input type="number" name="duration" class="durationField" placeholder="DURATION"/>'+
+                          '</div>')
+}
+
+$().ready(function(){
+  var data = fetchSettings()
+  if (data) {
+    drawMain(data)
+  } else {
+    showConfig()
+  }
+  $('#addBtn').click(function(){
+    addListItem()
+  })
+  
+  $('#editBtn').click(function(){
+    showConfig()
+  })
+});
